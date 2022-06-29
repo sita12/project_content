046ed240cc1446003383676eba5b225b4a7a6af2
sita <sitaneupane5700@gmail.com>
2022-06-29 17:27:51 +0545

    Jwt encode and decode method

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,27 @@
+class ApiController < ApplicationController
+    protect_from_forgery with: :null_session
+
+    private
+    def authorize_request
+        header = request.headers['Authorization']
+        header.nil? ? render_api_message("Error: Unauthorized Access", :unauthorized) : token_auth          
+    end
+
+    def token_auth
+        begin   
+            token = JsonWebToken.decode(header)
+            token.blank? ? render_api_message("Error: Token is expired or invalid", :unauthorized) : token_user(token)   
+        rescue JWT::DecodeError => e
+            render json: { message: e.message, status: 401}, status: :unauthorized
+        end  
+    end 
+    
+    def token_user(token)
+        @user = User.where(id: token[:user_id])  
+        render_api_message("Error: Token is expired or invalid", :unauthorized) if @user.blank?
+    end 
+    
+    def render_api_message message, status
+        render json: {message: message}, status: status
+    end
+end
